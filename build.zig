39b0cf3dbf8da575d4faaf1852a78a57e62ddf21
Tathya <coding.tathya@gmail.com>
2025-11-12 13:49:08 +0530

    fix: Redid build system

--- build.zig
+++ build.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
 
-const targets: []const std.Target.Query = &.{
-    .{ .cpu_arch = .aarch64, .os_tag = .macos },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
-    .{ .cpu_arch = .x86_64, .os_tag = .windows },
-    .{ .cpu_arch = .aarch64, .os_tag = .linux },
-};
-
 pub fn build(b: *std.Build) !void {
-    const make_all_targets = b.option(bool, "all", "Build for all supported targets") orelse false;
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -19,61 +9,33 @@ pub fn build(b: *std.Build) !void {
     const options = b.addOptions();
     options.addOption([]const u8, "version", version);
 
-    var reqd_exe: *std.Build.Step.Compile = undefined;
+    const exe = b.addExecutable(.{
+        .name = "3bp",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
 
-    if (make_all_targets) {
-        for (targets) |t| {
-            const exe = b.addExecutable(.{
-                .name = "3bp",
-                .root_module = b.createModule(.{
-                    .root_source_file = b.path("src/main.zig"),
-                    .target = b.resolveTargetQuery(t),
-                    .optimize = optimize,
-                }),
-            });
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
+    const raylib_artifact = raylib_dep.artifact("raylib");
 
-            exe.root_module.addOptions("config", options);
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
-            const triple: []const u8 = try t.zigTriple(b.allocator);
+    exe.root_module.addOptions("config", options);
 
-            const target_output = b.addInstallArtifact(exe, .{
-                .dest_dir = .{
-                    .override = .{
-                        .custom = triple,
-                    },
-                },
-            });
-
-            b.getInstallStep().dependOn(&target_output.step);
-
-            const curr_target = exe.root_module.resolved_target.?.result;
-
-            if (curr_target.cpu.arch == target.result.cpu.arch and
-                curr_target.os.tag == target.result.os.tag and
-                curr_target.abi == target.result.abi)
-            {
-                reqd_exe = exe;
-            }
-        }
-    } else {
-        const exe = b.addExecutable(.{
-            .name = "3bp",
-            .root_module = b.createModule(.{
-                .root_source_file = b.path("src/main.zig"),
-                .target = target,
-                .optimize = optimize,
-            }),
-        });
-
-        exe.root_module.addOptions("config", options);
-
-        b.installArtifact(exe);
-
-        reqd_exe = exe;
-    }
+    b.installArtifact(exe);
 
     // === Add run step ===
-    const run_cmd = b.addRunArtifact(reqd_exe);
+    const run_cmd = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the 3bp executable");
 
     run_step.dependOn(&run_cmd.step);
